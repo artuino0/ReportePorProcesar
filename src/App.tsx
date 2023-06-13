e3bb5e65e188b7c0a7af6ea38bef5143e227be51
artuino0 <37974162+artuino0@users.noreply.github.com>
2023-06-13 11:57:19 -0600

    Basic Structure
    Create a basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Cards from "./components/cards/Cards";
+import TableData from "./components/table/TableData";
 
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-red-50 flex items-center justify-center text-6xl font-bold">Prueba de TailwindCSS</div>
+      <Header />
+      <Cards />
+      <TableData />
     </>
   );
 }
