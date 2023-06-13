5be058509c7f54d8d07cbd67bb7030e443f1e2c7
artuino0 <37974162+artuino0@users.noreply.github.com>
2023-06-13 11:51:39 -0600

    First Commit
    App created with VITE React + TS and TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen bg-red-50 flex items-center justify-center text-6xl font-bold">Prueba de TailwindCSS</div>
+    </>
+  );
+}
+
+export default App;
