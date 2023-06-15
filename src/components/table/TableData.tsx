a204ee059738398c68a5f622a06792637271a7a4
AngelFmc <angelfmc01@hotmail.com>
2023-06-15 16:18:06 -0600

    Creat display button and design update
    Se creo un botón para desplegar la información y se arreglaron errores de diseño

--- src/components/table/TableData.tsx
+++ src/components/table/TableData.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import formatearNumeroMexicano from "../../helpers/formater";
 import Recepcion from "../../interfeces/receptions";
-import tree from "../../assets/tree.svg";
 import { EXCLUIDOS } from "../../global";
 
 interface IProps {
@@ -10,27 +9,39 @@ interface IProps {
 
 const TableData = (props: IProps) => {
   const { recepciones } = props;
-  const [levelOpen, setLevelOpen] = useState<number>(0);
+  const [mutatedRecepciones, setMutatedRecepciones] = useState<Recepcion[]>([]);
 
-  const handleOpener = () => {
-    if (levelOpen == 3) {
-      setLevelOpen(0);
-      return;
-    }
-    setLevelOpen(levelOpen + 1);
-    console.log(levelOpen);
+  useEffect(() => {
+    setMutatedRecepciones(recepciones);
+  });
+
+  const handleOpenClick = (data: string) => {
+    const tranformerData = mutatedRecepciones?.map((recepcion) => {
+      if (recepcion.cultivo === data) {
+        recepcion.detailed = !recepcion.detailed;
+      }
+
+      recepcion.agricolas.map((agricola) => {
+        if (agricola.agricola === data) {
+          agricola.detailed = !agricola.detailed;
+        }
+
+        agricola.lotes.map((lote) => {
+          if (lote.lote === data) {
+            lote.detailed = !lote.detailed;
+          }
+        });
+      });
+
+      return recepcion;
+    });
+
+    setMutatedRecepciones(tranformerData);
   };
 
   return (
     <>
       <div className="w-full m-auto px-3 mb-10 relative ">
-        <button
-          className="fixed bottom-3 right-3 bg-gray-400 rounded-full h-14 w-14 shadow-md flex items-center justify-center"
-          onClick={handleOpener}
-        >
-          <img src={tree} alt="" className="w-8 h-8" />
-        </button>
-
         <table className=" bg-white w-full shadow-md rounded-xl text-sm p-5 overflow-hidden">
           <thead>
             <tr className="border-b">
@@ -45,17 +56,29 @@ const TableData = (props: IProps) => {
           </thead>
 
           <tbody>
-            {recepciones.map((recepcion) => (
+            {mutatedRecepciones.map((recepcion) => (
               <>
                 {EXCLUIDOS.includes(recepcion.cultivo.split(" ")[0]) ? null : (
                   <>
                     <tr
                       className={
-                        levelOpen >= 1 ? "font-bold" : "even:bg-gray-50"
+                        recepcion.detailed ? "font-bold" : "even:bg-gray-50"
                       }
                       key={recepcion.cultivo}
                     >
                       <td className="py-[3.5px] text-left pl-6">
+                        <button
+                          className="h-5 w-5 items-center justify-center mx-1"
+                          onClick={() => handleOpenClick(recepcion.cultivo)}
+                        >
+                          <i
+                            className={
+                              recepcion.detailed
+                                ? "bi bi-dash-square w-2 h-2"
+                                : "bi bi-plus-square w-2 h-2"
+                            }
+                          ></i>
+                        </button>
                         {recepcion.cultivo}
                       </td>
                       <td className="text-left">
@@ -78,131 +101,168 @@ const TableData = (props: IProps) => {
                       <td className="text-left">-</td>
                     </tr>
 
-                    {recepcion.agricolas.map((agricola) => (
-                      <>
+                    {recepcion.detailed &&
+                      recepcion.agricolas.map((agricola) => (
                         <>
-                          {levelOpen >= 1 ? (
-                            <tr
-                              key={agricola.agricola}
-                              className={
-                                levelOpen >= 2
-                                  ? "font-semibold bg-slate-100"
-                                  : "even:bg-gray-50"
-                              }
-                            >
-                              <td className="py-1 text-left pl-11">
-                                {agricola.agricola}
-                              </td>
-                              <td className="text-left">
-                                {formatearNumeroMexicano(agricola.noProcesado)}
-                              </td>
-                              <td className="text-left">
-                                {formatearNumeroMexicano(
-                                  agricola.corriendo.procesado
-                                )}
-                              </td>
-                              <td className="text-left">
-                                {formatearNumeroMexicano(
-                                  agricola.corriendo.porProcesar
-                                )}
-                              </td>
-                              <td className="text-left">
-                                {formatearNumeroMexicano(agricola.completado)}
-                              </td>
-                              <td className="text-left">
-                                {formatearNumeroMexicano(
-                                  agricola.total_para_empacar
-                                )}
-                              </td>
-                              <td className="text-left">-</td>
-                            </tr>
-                          ) : null}
-                        </>
-
-                        {agricola.lotes.map((lote) => (
                           <>
-                            <>
-                              {levelOpen >= 2 ? (
-                                <tr
-                                  key={lote.lote}
-                                  className={
-                                    levelOpen >= 3
-                                      ? "font-semibold bg-orange-100"
-                                      : "even:bg-gray-50"
-                                  }
-                                >
-                                  <td className="py-1 text-left pl-16">
-                                    {lote.lote}
-                                  </td>
-                                  <td className="text-left">
-                                    {formatearNumeroMexicano(lote.noProcesado)}
-                                  </td>
-                                  <td className="text-left">
-                                    {formatearNumeroMexicano(
-                                      lote.corriendo.procesado
-                                    )}
-                                  </td>
-                                  <td className="text-left">
-                                    {formatearNumeroMexicano(
-                                      lote.corriendo.porProcesar
-                                    )}
-                                  </td>
-                                  <td className="text-left">
-                                    {formatearNumeroMexicano(lote.completado)}
-                                  </td>
-                                  <td className="text-left">
-                                    {formatearNumeroMexicano(
-                                      lote.total_para_empacar
-                                    )}
-                                  </td>
-                                  <td className="text-left">-</td>
-                                </tr>
-                              ) : null}
-                            </>
+                            {mutatedRecepciones ? (
+                              <tr
+                                key={agricola.agricola}
+                                className={
+                                  agricola.detailed
+                                    ? "font-semibold bg-slate-100"
+                                    : "even:bg-gray-50"
+                                }
+                              >
+                                <td className="py-1 text-left pl-11">
+                                  <button
+                                    className="h-5 w-5 items-center justify-center mx-1"
+                                    onClick={() =>
+                                      handleOpenClick(agricola.agricola)
+                                    }
+                                  >
+                                    <i
+                                      className={
+                                        agricola.detailed
+                                          ? "bi bi-dash-square w-2 h-2"
+                                          : "bi bi-plus-square w-2 h-2"
+                                      }
+                                    ></i>
+                                  </button>
+                                  {agricola.agricola}
+                                </td>
+                                <td className="text-left">
+                                  {formatearNumeroMexicano(
+                                    agricola.noProcesado
+                                  )}
+                                </td>
+                                <td className="text-left">
+                                  {formatearNumeroMexicano(
+                                    agricola.corriendo.procesado
+                                  )}
+                                </td>
+                                <td className="text-left">
+                                  {formatearNumeroMexicano(
+                                    agricola.corriendo.porProcesar
+                                  )}
+                                </td>
+                                <td className="text-left">
+                                  {formatearNumeroMexicano(agricola.completado)}
+                                </td>
+                                <td className="text-left">
+                                  {formatearNumeroMexicano(
+                                    agricola.total_para_empacar
+                                  )}
+                                </td>
+                                <td className="text-left">-</td>
+                              </tr>
+                            ) : null}
+                          </>
 
-                            {lote.recepciones.map((recep) => (
+                          {agricola.detailed &&
+                            agricola.lotes.map((lote) => (
                               <>
-                                {levelOpen >= 3 ? (
-                                  <tr className="even:bg-gray-50">
-                                    <td className="py-1 text-left pl-24">
-                                      {recep.recepcion}
-                                    </td>
-                                    <td className="text-left">
-                                      {formatearNumeroMexicano(
-                                        recep.noProcesado
-                                      )}
-                                    </td>
-                                    <td className="text-left">
-                                      {formatearNumeroMexicano(
-                                        recep.corriendo.procesado
-                                      )}
-                                    </td>
-                                    <td className="text-left">
-                                      {formatearNumeroMexicano(
-                                        recep.corriendo.porProcesar
-                                      )}
-                                    </td>
-                                    <td className="text-left">
-                                      {formatearNumeroMexicano(
-                                        recep.completado
-                                      )}
-                                    </td>
-                                    <td className="text-left">
-                                      {formatearNumeroMexicano(
-                                        recep.total_para_empacar
-                                      )}
-                                    </td>
-                                    <td className="text-left">
-                                      {recep.dias_piso}
-                                    </td>
-                                  </tr>
-                                ) : null}
+                                <>
+                                  {mutatedRecepciones ? (
+                                    <tr
+                                      key={lote.lote}
+                                      className={
+                                        lote.detailed
+                                          ? "font-semibold bg-orange-100"
+                                          : "even:bg-gray-50"
+                                      }
+                                    >
+                                      <td className="py-1 text-left pl-16">
+                                        <button
+                                          className="h-5 w-5 items-center justify-center mx-1"
+                                          onClick={() =>
+                                            handleOpenClick(lote.lote)
+                                          }
+                                        >
+                                          <i
+                                            className={
+                                              lote.detailed
+                                                ? "bi bi-dash-square w-2 h-2"
+                                                : "bi bi-plus-square w-2 h-2"
+                                            }
+                                          ></i>
+                                        </button>
+                                        {lote.lote}
+                                      </td>
+                                      <td className="text-left">
+                                        {formatearNumeroMexicano(
+                                          lote.noProcesado
+                                        )}
+                                      </td>
+                                      <td className="text-left">
+                                        {formatearNumeroMexicano(
+                                          lote.corriendo.procesado
+                                        )}
+                                      </td>
+                                      <td className="text-left">
+                                        {formatearNumeroMexicano(
+                                          lote.corriendo.porProcesar
+                                        )}
+                                      </td>
+                                      <td className="text-left">
+                                        {formatearNumeroMexicano(
+                                          lote.completado
+                                        )}
+                                      </td>
+                                      <td className="text-left">
+                                        {formatearNumeroMexicano(
+                                          lote.total_para_empacar
+                                        )}
+                                      </td>
+                                      <td className="text-left">-</td>
+                                    </tr>
+                                  ) : null}
+                                </>
+
+                                {lote.detailed &&
+                                  lote.recepciones.map((recep) => (
+                                    <>
+                                      {mutatedRecepciones ? (
+                                        <tr className="even:bg-gray-50">
+                                          <td className="py-1 text-left pl-24">
+                                            {recep.recepcion}
+                                          </td>
+                                          <td className="text-left">
+                                            {formatearNumeroMexicano(
+                                              recep.noProcesado
+                                            )}
+                                          </td>
+                                          <td className="text-left">
+                                            {formatearNumeroMexicano(
+                                              recep.corriendo.procesado
+                                            )}
+                                          </td>
+                                          <td className="text-left">
+                                            {formatearNumeroMexicano(
+                                              recep.corriendo.porProcesar
+                                            )}
+                                          </td>
+                                          <td className="text-left">
+                                            {formatearNumeroMexicano(
+                                              recep.completado
+                                            )}
+                                          </td>
+                                          <td className="text-left">
+                                            {formatearNumeroMexicano(
+                                              recep.total_para_empacar
+                                            )}
+                                          </td>
+                                          <td className="text-left">
+                                            {recep.dias_piso}
+                                          </td>
+                                        </tr>
+                                      ) : null}
+                                    </>
+                                  ))}
                               </>
                             ))}
-                          </>
-                        ))}
-                      </>
-                    ))}
+                        </>
+                      ))}
                   </>
                 )}
               </>
